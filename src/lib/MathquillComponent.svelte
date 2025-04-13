c756078258112b7e5d39936c0753e1dd96163511
Ivan Lott <88263600+Tartarus6@users.noreply.github.com>
2025-04-13 11:40:42 -0400

    Inline Latex
    * added mathquill inline
    
    * fixed mathquill, no longer using svelte
    
    * fixed math inline decorations
    
    * mathInline settings for spaceBehavesLikeTab and autoCommands
    
    * loading previousely open note on startup
    
    * added tab behaviour to code blocks
    
    * removed unils debug
    
    * ability to enter mathInline with arrow keys
    
    * fixed possible issue preventing inlineMath saving
    
    * fixed desync between inlineMath cursor side
    
    * added escaping mathInline with arrow keys
    
    * added task lists, kinda broken
    
    * mathInline focus on create and delete if empty

---
+++ src/lib/MathquillComponent.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" module>
+	import type { MathQuillStatic } from '$lib/mathquill-desmos/mathquill';
+
+	declare const MathQuill: MathQuillStatic; // Use declare, but don't import
+</script>
+
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let mathFieldSpan: HTMLSpanElement;
+
+	onMount(async () => {
+		const MQ = MathQuill.getInterface(3);
+
+		if (mathFieldSpan) {
+			MQ.MathField(mathFieldSpan, {
+				spaceBehavesLikeTab: true,
+				autoCommands: 'pi theta sqrt sum choose int',
+				handlers: {
+					edit: () => {}
+				}
+			});
+		}
+	});
+</script>
+
+<span bind:this={mathFieldSpan}></span>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,9 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<link rel="stylesheet" href="/src/lib/mathquill-desmos/mathquill.css" />
+	<script src="/src/lib/mathquill-desmos/mathquill.js"></script>
+</svelte:head>
+
 {@render children()}
